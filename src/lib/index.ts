a0b2d1f4e4e02dbdd85c8c0b3ddefc3cd77208d6
Kris Johansen <kris.johansen@gmail.com>
2022-04-11 09:18:51 +1200

    WIP

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,3 +6,19 @@ export { SMTP2GOApiClient as ApiClient };
 export { SMTP2GOService as Service };
 export { MailService };
 
+export default function SMTP2GOApi(apiKey: string) {
+    return {
+        service: function (endpoint: string) {
+            return new SMTP2GOService(endpoint);
+        },
+        mail: function () {
+            return new MailService();
+        },
+        client: function () {
+            return new SMTP2GOApiClient(apiKey);
+        }
+    }
+}
+
+
+

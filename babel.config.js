1fe9f8f6c473dc371b0d7ac6644b0a3c8d68596f
Kris Johansen <kris.johansen@gmail.com>
2023-12-04 11:01:02 +1300

    config update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [["@babel/env"]],
   plugins: [
-    ["@babel/plugin-proposal-class-properties"],
+    ["@babel/plugin-transform-class-properties"],
     ["@babel/plugin-transform-typescript"],
     ["@babel/plugin-transform-runtime"],
   ],
